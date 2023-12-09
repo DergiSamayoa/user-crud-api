c4035f7bccdfe674760b6e76600bd8aaa3c34947
Dergi Samayoa <dergio@gmail.com>
2023-12-09 14:45:01 -0600

    Include Daysi UI & timer to delivery

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
+import Timer from "./components/Timer";
 import ListUser from "./components/ListUser";
 
 function App() {
   return (
     <main className="bg-black/70 min-h-screen w-[1200px] m-auto">
       <header className="flex flex-row justify-between align-middle p-4">
-        <h1 className="text-center p-2 font-bold text-2xl">CRUD - User with api</h1>
-        <div className="col-span-2">Seach by name <button className="p-2 border-2 bg-blue-500 text-black rounded-2xl ml-4">User New</button></div>
+        <h1 className="text-center p-2 font-semibold text-2xl">
+          User - <span className="font-bold">crud</span> with api
+        </h1>
+        <div className="flex flex-row items-center">
+          <div className="border-1 rounded-2xl bg-white">
+            <input 
+              className="pl-2 outline-none rounded-2xl w-[320px]" 
+              type="text" placeholder="Seach by name" />
+            <button className="p-2 border-2 bg-blue-500 text-black rounded-2xl">
+              Search
+            </button>
+          </div>
+          <button className="p-2 border-2 bg-blue-500 text-black rounded-2xl ml-4">
+            Add user
+          </button>
+        </div>
       </header>
       <article className="mt-10  bg-cyan-700 m-auto text-center">
+          <Timer />
           <ListUser />
       </article>
     </main>
