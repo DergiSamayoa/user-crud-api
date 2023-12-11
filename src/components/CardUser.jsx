695d03442d9dea0b539e3bf1d1bf8a8d6b7ca5ae
Jesús Aguilar <aguilaraliagajesus@gmail.com>
2023-12-11 02:37:09 -0400

    animaciones listas

--- src/components/CardUser.jsx
+++ src/components/CardUser.jsx
@@ -9,7 +9,6 @@ import variants from "../utils/VariantsFM";
 let variantCard = variants.card
 
 const CardUSer = (currentUser) => {
-  const user = currentUser.currentUser
   return (
     <motion.article variants={variantCard} initial="hidden" animate="visible" exit="exit" className="relative w-[380px] h-[275px] bg-white text-[#0f0f2d] flex flex-col p-5 gap-3 border-[1px] rounded-md border-[#e1e1e1] max-sm:w-[350px] max-sm:h-[245px] max-sm:p-3">
       <h3 className="text-[25px] font-medium">{currentUser.first_name} {currentUser.last_name}</h3>
@@ -17,11 +16,11 @@ const CardUSer = (currentUser) => {
         <h4 className="font-[#d3d3d3] opacity-50"></h4>
         <p className="font-medium">{currentUser.email}</p>
       </div>
-      <div className="w-full flex flex-col">
-        <h4 className="font-[#d3d3d3] opacity-50">CUMPLEAÑOS</h4>
-        <p className="font-medium flex gap-2"><IconGift />{user?.bithday}</p>
+      <div className="w-full flex flex-col items-start">
+        <h4 className="font-[#d3d3d3] opacity-50"></h4>
+        <p className="font-medium flex gap-2"><IconGift />{currentUser?.bithday}</p>
       </div>
-      <div className="absolute bottom-4 right-4 flex gap-4 ">
+      <div className="absolute bottom-4 right-4 flex gap-4">
         <button 
             onClick={() => SaveUser("delete", currentUser)}
             className="w-[43px] h-[43px] bg-[#d85d5d] text-white rounded-md flex items-center justify-center">
--- src/components/ListUser.jsx
+++ src/components/ListUser.jsx
@@ -1,20 +1,20 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import CardUser from "./CardUser";
+import { useEffect, useState } from "react"
+import axios from "axios"
 import SaveUser from "./SaveUser"
+import CardUser from "./CardUser"
 import { AnimatePresence } from "framer-motion"
 
 const ListUser = () => {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([])
 
   useEffect(() => {
     axios
-      .get("https://users-crud.academlo.tech/users/")
-      .then((users) => setUsers(users.data))
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+      .get('https://users-crud.academlo.tech/users/')
+      .then(users => setUsers(users.data))
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
 
   return (
     <article className="w-full grid grid-cols-3 gap-12 max-xl:grid-cols-2 max-lg:grid-cols-1 max-sm:gap-6">
@@ -27,4 +27,4 @@ const ListUser = () => {
   )
 }
 
-export default ListUser;
+export default ListUser
