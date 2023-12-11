cdf563953539dfbbf50849aca902a3e361ebc14c
Jesús Aguilar <aguilaraliagajesus@gmail.com>
2023-12-11 01:45:33 -0400

    nuevo commit

--- src/components/ListUser.jsx
+++ src/components/ListUser.jsx
@@ -1,20 +1,21 @@
-import { useEffect, useState } from "react"
-import axios from "axios"
+import { useEffect, useState } from "react";
+import axios from "axios";
+import CardUser from "./CardUser";
 import SaveUser from "./SaveUser"
 import CardUser from "./CardUser"
 import { AnimatePresence } from "framer-motion"
 
 const ListUser = () => {
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     axios
-      .get('https://users-crud.academlo.tech/users/')
-      .then(users => setUsers(users.data))
-      .catch(err => {
-        console.log(err)
-      })
-  }, [])
+      .get("https://users-crud.academlo.tech/users/")
+      .then((users) => setUsers(users.data))
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <article className="w-full grid grid-cols-3 gap-12 max-xl:grid-cols-2 max-lg:grid-cols-1 max-sm:gap-6">
@@ -27,4 +28,4 @@ const ListUser = () => {
   )
 }
 
-export default ListUser
+export default ListUser;
